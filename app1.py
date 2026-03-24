b4825cb24790ae12b03fc7731243739e9a32bc84
Pfyber <luka.colaric@gmail.com>
2026-03-24 09:07:13 +0000

    3.rb

--- app1.py
+++ app1.py
@@ -2,14 +2,13 @@ from flask import Flask, render_template, request, redirect, session, jsonify
 from tinydb import TinyDB, Query
 
 app = Flask(__name__)
-app.secret_key = "skrivnost123"
+app.secret_key = "karkoliToleJeSkrivnost123"
 
 db = TinyDB("db.json")
 users = db.table("users")
 
 User = Query()
 
-# homepage
 @app.route("/")
 def home():
     if "user" in session:
@@ -21,30 +20,27 @@ def register():
     if request.method == "POST":
         username = request.form["username"]
         password = request.form["password"]
-
-        if users.search(User.username == username):
-            return "Uporabnik že obstaja"
-
-        users.insert({"username" : username, "password": password, "note" : ""})
-        return redirect("/login")
         #print(username, password)
+        if users.search(User.username == username):
+            return redirect("/register")
+        else:
+            users.insert({"username" : username, "password": password, "note" : ""})
+            return redirect("/login")
+
     return render_template("register.html")
 
 @app.route("/login", methods = ["GET", "POST"])
 def login():
-
     if request.method == "POST":
         username = request.form["username"]
         password = request.form["password"]
 
-        user = users.get(User.username == username)
+        user = users.get(User.username == username )
         #print(user)
-
         if user and user["password"] == password:
             session["user"] = username
             return redirect("/dashboard")
-        
-        return "Napačen login"
+
 
     return render_template("login.html")
 
@@ -55,4 +51,4 @@ def dashboard():
     
     return render_template("dashboard.html", user = session["user"])
 
-app.run(debug=1)
+app.run(debug=True)
