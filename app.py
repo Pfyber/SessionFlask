e3ada71442597521316acb13c8743daf2c3f0dfd
Pfyber <luka.colaric@gmail.com>
2026-03-25 06:59:45 +0000

    3.rc final commit

--- app.py
+++ app.py
@@ -2,14 +2,13 @@ from flask import Flask, render_template, request, redirect, session, jsonify
 from tinydb import TinyDB, Query
 
 app = Flask(__name__)
-app.secret_key = "skrivnost123"
+app.secret_key = "karkoliToleJeSkrivnost123"
 
 db = TinyDB("db.json")
 users = db.table("users")
 
 User = Query()
 
-# homepage
 @app.route("/")
 def home():
     if "user" in session:
@@ -21,30 +20,27 @@ def register():
     if request.method == "POST":
         username = request.form["username"]
         password = request.form["password"]
-
-        if users.search(User.username == username):
-            return "Uporabnik že obstaja"
-
-        users.insert({"username" : username, "password": password, "note" : ""})
-        return redirect("/login")
         #print(username, password)
+        if users.search(User.username == username):
+            return redirect("/register")
+        else:
+            users.insert({"username" : username, "password": password, "note" : ""})
+            return redirect("/login")
+
     return render_template("register.html")
 
 @app.route("/login", methods = ["GET", "POST"])
 def login():
-
     if request.method == "POST":
         username = request.form["username"]
         password = request.form["password"]
 
-        user = users.get(User.username == username)
+        user = users.get(User.username == username )
         #print(user)
-
         if user and user["password"] == password:
             session["user"] = username
             return redirect("/dashboard")
-        
-        return "Napačen login"
+
 
     return render_template("login.html")
 
@@ -52,7 +48,20 @@ def login():
 def dashboard():
     if "user" not in session:
         return redirect("/login")
-    
-    return render_template("dashboard.html", user = session["user"])
+    user = users.get(User.username == session["user"] )
+    return render_template("dashboard.html", user = session["user"], text = user["note"])
 
-app.run(debug=1)
+# shrani note
+@app.route("/saveNote", methods = ["POST"])
+def saveNote():
+    note = request.form["text"]
+    users.update({"note": note}, User.username == session["user"] )
+
+    return "SAVED"
+
+
+@app.route("/logout")
+def logout():
+    session.clear()
+    return redirect("/login")
+app.run(debug=True)
